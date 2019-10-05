80a77623f60ae381dc4df9c0033e9ef39b2fd773
Kevin Steele <kevdsteele@gmail.com>
2019-10-05 14:27:51 -0400

    redux state for event component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer.js";
 import errorReducer from "./errorReducer.js";
+import eventsReducer from "./eventsReducer.js";
 
 export default combineReducers({
   auth: authReducer,
-  error: errorReducer
+  error: errorReducer,
+ events: eventsReducer
 });
