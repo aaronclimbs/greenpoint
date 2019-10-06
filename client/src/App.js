d48a6a41c80d31cbe0ae1209cc01624e9dc87381
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2019-10-06 15:22:40 -0400

    Added the Resources page, started working on the Why page and started to structure the components folders.

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import AppNavbar from "./components/AppNavbar";
-import Why from "./components/pages/Why";
-import FAQ from "./components/pages/FAQ";
+import Why from "./components/pages/Why/Why";
+import Resources from "./components/pages/Resources/Resources";
+import FAQ from "./components/pages/FAQ/FAQ";
 import Profile from "./components/pages/Profile";
 import NotFound from "./components/pages/NotFound";
 import Landing from "./components/pages/Landing";
 import EventsList from "./components/events/";
+
 
 class App extends Component {
   render() {
@@ -22,6 +24,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" render={() => (isAuthenticated ? <Redirect to="/profile" /> : <Landing />)} />
             <Route path="/profile" render={() => (isAuthenticated ? <Profile /> : <Redirect to="/" />)} />
+            <Route path="/resources" component={Resources} />
             <Route path="/faq" component={FAQ} />
             <Route path="/why" component={Why} />
             <Route path="/events" component={EventsList} />
