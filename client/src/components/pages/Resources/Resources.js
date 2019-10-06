26e713b4618daf66ffab9a4ca941679f9e11663f
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2019-10-06 15:22:40 -0400

    Added the Resources page, started working on the Why page and started to structure the components folders.

---
+++ client/src/components/pages/Resources/Resources.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Resources.css"
+
+export default class Resources extends Component {
+  render() {
+    return (
+      <div>
+        <h5 className="text-center mt-2 mb-2">Resources</h5>
+      </div>
+    );
+  }
+}
