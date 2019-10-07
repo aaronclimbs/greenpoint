3122bcce1175fac716b32f6f12ac38d182b4a4ca
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-10-07 17:00:42 -0400

    collapsible FAQs - needs styling and some other jazz but works

--- client/src/components/pages/FAQ/FAQ.js
+++ client/src/components/pages/FAQ/FAQ.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import qsAndAs from './question-answers';
+import Question from './Question';
 
 export default class FrequentlyAsked extends Component {
+
+  state = {
+    questions: qsAndAs
+  };
+
+  // loadFAQs = () => {
+  //   this.setState({ questions: qsAndAs });
+  // };
+  
   render() {
     return (
       <div>
         <Container className="faq-container">
-          
+          <Row>
+            <Col>
+              <h1>Frequently Asked Questions</h1>
+              <br></br>
+              <br></br>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              {/* Render FAQs here */}
+              <ul className="question-list">
+                {Object.keys(this.state.questions).map(key =>
+                  <Question 
+                    key={key}
+                    questions={this.state.questions[key]}
+                  />
+                )}
+              </ul>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import '../src/components/pages/FAQ/faq.css'
 
 import Root from "./Root";
 
