b4eefd94fbea6297cb6c9a06dc0d0109f691d05e
davidpom <davidpom@gmail.com>
2019-10-07 20:58:45 -0400

    charts: doughnut and line

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,14 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer.js";
 import errorReducer from "./errorReducer.js";
 import eventsReducer from "./eventsReducer.js";
+import doughnutReducer from "./doughnutReducer.js";
+import lineReducer from "./lineReducer.js";
 
 export default combineReducers({
   auth: authReducer,
   error: errorReducer,
- events: eventsReducer
+  events: eventsReducer,
+  doughut: doughnutReducer,
+  line: lineReducer
+
 });
