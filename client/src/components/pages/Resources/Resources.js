d53ef48adb4d504d3d1a572802a29ee15ee1f69e
Javier <javierjpagan@gmail.com>
2019-10-08 19:48:05 -0400

    Started scraping in Resources page

--- client/src/components/pages/Resources/Resources.js
+++ client/src/components/pages/Resources/Resources.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import scraper from "../../../helpers/scraper"
 import "./Resources.css"
 
 export default class Resources extends Component {
-  render() {
+
+  componentDidMount() {
+scraper();
+  }
+
+
+
+
+render() {
+    // Page Render
     return (
       <div>
         <h5 className="text-center mt-2 mb-2">Resources</h5>
