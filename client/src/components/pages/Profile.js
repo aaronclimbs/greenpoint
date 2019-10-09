bd81cd7118f95d777ba1ae36c6cb2670b501915c
Kevin Steele <kevdsteele@gmail.com>
2019-10-09 19:58:12 -0400

    MVP login in the event.js file
    models, routes, controller changed as well fro logs

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import EventList from '../events'
 
 class Profile extends Component {
   render() {
-    return <div>Welcome to your profile, {this.props.auth.user.name}</div>;
+
+    return <div>
+    <div>Welcome to your profile, {this.props.auth.user.name} </div>
+    <EventList userID={this.props.auth.user._id}/>
+    </div>
   }
 }
 
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,12 +1,33 @@
 const axios = require('axios');
 const db = require("../models");
+var ObjectId = require('mongoose').Types.ObjectId
+
+var today = Date.now()
+
+console.log("Today is" + today)
 
 
 module.exports = {
   findAll: function(req, res) {
     db.Log
       .find(req.query)
+      .populate( "eventID")
       .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findByIdDate: function(req, res) {
+    console.log("Findy by ID Date User id is " + req.params.id)
+    console.log("Findy by ID Date is " + req.params.date)
+    db.Log
+      .aggregate([
+      {$match: { $and: [{userID:ObjectId(req.params.id)}, {eventDate: req.params.date}]}},  
+      // {$group: {
+      //   _id:"$eventCat",
+      //   totalPoints: {$sum: {$multiply: ["$eventQuantity",'$eventPoints']}}
+      // }}
+    
+    ])
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
@@ -16,6 +37,23 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  groupByEventDate: function(req, res) {
+    console.log("User id is " + req.params.id)
+    console.log("Date is " + req.params.date)
+    db.Log
+      .aggregate([
+      {$match: { $and: [{userID:ObjectId(req.params.id)}, {eventDate: req.params.date}]}},  
+      {$group: {
+        _id:"$eventCat",
+        totalPoints: {$sum: {$multiply: ["$eventQuantity",'$eventPoints']}}
+      }},
+      {$sort: {totalPoints: -1}}
+    
+    ])
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  
   create: function(req, res) {
     db.Log
       .create(req.body)
@@ -23,12 +61,15 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
+    console.log("received an update for " + req.params.id)
+    console.log("update data is " + req.body)
     db.Log
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
+    console.log("Items to delete is " + req.params.id)
     db.Log
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
