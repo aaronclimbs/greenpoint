b6962b216236d00ef4e0f3dd3adbb50348ba6f1f
Kevin Steele <kevdsteele@gmail.com>
2019-10-10 19:50:11 -0400

    Integrating Doughnut chart
    Need to look at update issue

--- client/src/components/doughnut/index.js
+++ client/src/components/doughnut/index.js
@@ -5,25 +5,9 @@ import { Form, FormGroup, Label, Row,Col} from "reactstrap";
 import { connect } from "react-redux";
 import { loadLogs } from "../../actions/doughnutActions"
 import moment from "moment";
-
-
 import axios from 'axios';
-
-	// const data = 
-const getMonth = moment().format('YYYY-MM-DD')
-console.log(getMonth);
-
-const  fetchLogs = async () => {
-    // const result = await axios.get(`api/logs/group/5d98d50d96ba210da4c0e3b0/${getMonth}`);
-    const result = await axios.get("api/logs/group/5d98d50d96ba210da4c0e3b0/" + getMonth)
-
-    // this.setState({ data : result.data, error: "" });
-    console.log(result.data);
-};
-
-
 const setData = {
-  labels: ["re-use", "food", "transportation", "lifestyle", "green action"],
+  labels: [],
   datasets: [
     {
       data: [],
@@ -38,158 +22,42 @@ const setData = {
     }
   ]
 };
-
 class DoughnutChart extends Component {
   state = {
-    data: []
+    labels: this.props.labels,
+    data: this.props.data
   };
-  // dropdownOpen: false,
-  // dropdownValue: "Choose a Green Action",
-  // doughnutData: {
-  //   labels: ["re-use", "food", "transportation", "lifestyle", "green action"],
-  //   datasets: [
-  //     {
-  //       data: [],
-  //       backgroundColor: [
-  //         "#FF6384",
-  //         "#36A2EB",
-  //         "#FFCE56",
-  //         "#FF7F50",
-  //         "#0EBFE9"
-  //       ],
-  //       hoverBackgroundColor: [
-  //         "#FF6384",
-  //         "#36A2EB",
-  //         "#FFCE56",
-  //         "#FF7F50",
-  //         "#0EBFE9"
-  //       ]
-  //     }
-  //   ]
-  // },
-  // doughnutArray: [],
-  // selected: [],
-  // startDate: new Date()
-  // };
-
-  // fetchLogs = async () => {
-  //   const result = await axios.get(`api/logs/:userId${this.state.search}`);
-  //   this.setState({ doughnutArray: result.data, error: "" });
-  // };
-
-  // onChange = e => {
-  //   this.setState({
-  //     [e.target.name]: e.target.value
-  //   });
-  // };
-
-  // handleDateChange = date => {
-  //   this.setState({
-  //     startDate: date
-  //   });
-  // };
-
-  // onClick = e => {
-  //   console.log(e.target);
-  //   console.log(e.currentTarget);
-  //   e.preventDefault();
-  //     console.log("Item list click" + e.currentTarget.catname)
-  //     // console.log("Target is " + JSON.stringify(e.target))
-  //     // console.log("Target is " + JSON.stringify(e.currentTarget))
-  //     const newSelect = {
-  //       id: e.currentTarget.id,
-  //       name: e.currentTarget.name,
-  //       cat: e.currentTarget.getAttribute('data-category'),
-  //       quantity: ""
-  //     }
-  //   this.setState({
-  //       selected: [...this.state.selected, newSelect]
-  //   }, () => {
-  //       this.setState({
-  //           dropdownValue: this.state.selected.length > 0 ? "Choose another Green Action" : "Choose a Green Action"
-  //       })
-  //   });
-  // };
-
-  //   delListItem = e => {
-  //     e.preventDefault()
-  //     const itemLoc = e.currentTarget.getAttribute("data-id")
-
-  //     const Select = this.state.selected
-  //     console.log("Select is " + Select)
-  //     const newSelect = Select.splice(itemLoc,1)
-  //     console.log("Select is now " + Select )
-
-  //    this.setState({
-  //      selected: Select
-  //    })
-  //   }
-  // componentDidUpdate() {
-
-  // }
-
-// console.log(getMonth);
-
-
-
-
   componentDidMount() {
 
+this.props.getTodayStats()
+setData.labels = this.state.labels;
+setData.datasets[0].data = this.state.data;
 
-
-
-    // setTimeout(
-    //   () =>
-    fetchLogs();
-
-
+    // fetchLogs();
     this.setState(
-      {
-        data: [3, 14, 2, 3, 20]
-      },
-      () => {
-        console.log(this.state.data);
-        setData.datasets[0].data = this.state.data;
-      }
+      // {
+      //   labels: ["food", "green action", "lifestyle", "re-use", "transportation"],
+      //   data: [3, 14, 2, 0, 23]
+      // },
+      // () => {
+      //   console.log(this.state.data);
+      //   setData.labels = this.state.labels;
+      //   setData.datasets[0].data = this.state.data;
+      // }
     );
-    //   500
-    // );
+    console.log("Labels are " + this.props.labels)
+    console.log("Data is" + this.props.data)
 
-    // data.datasets.data = this.state.doughnutData;
-    // console.log(data.datasets.data);
-    // console.log(this.state.doughnutData);
-    // const dataWrapper = this.state.doughnutData;
-    // console.log(this.state.doughnutData.datasets[0].data);
-    // dataWrapper.datasets[0].data = [
-    //   ...this.state.doughnutData.datasets[0].data,
-    //   12,
-    //   3,
-    //   45,
-    //   60,
-    //   20
-    // ];
-    // console.log(dataWrapper.datasets[0]);
-    // this.setState(
-    //   {
-    //     doughnutData: [dataWrapper]
-    //   },
-    //   () => console.log(this.state.doughnutData)
-    // );
-    // this.fetchLogs();
-    // this.props.loadLogs();
+
+    // setData.labels = this.props.labels;
+    // setData.datasets[0].data = this.props.data;
+       
   }
-
-  // toggle = () => {
-  //     this.setState ({
-  //       dropdownOpen: !this.state.dropdownOpen
-  //     });
-  //   }
-
   render() {
     console.log(setData);
     return (
       <div>
-        <h2>Doughnut Example</h2>
+        
         <div className="doughnut_chart">
           <Doughnut data={setData} />
         </div>
@@ -197,29 +65,9 @@ class DoughnutChart extends Component {
     );
   }
 }
-
-// const mapStateToProps = state => ({
-//   events: state.events
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const mapStateToProps = state => ({
     events: state.events
-
 });
-
 export default connect(
     mapStateToProps,
     { loadLogs }
