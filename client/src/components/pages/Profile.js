57efddc179fec2ac23ba22fe08e293080bddc8c8
Kevin Steele <kevdsteele@gmail.com>
2019-10-09 20:00:16 -0400

    Revert "MVP login in the event.js file"
    This reverts commit bd81cd7118f95d777ba1ae36c6cb2670b501915c.

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import EventList from '../events'
 
 class Profile extends Component {
   render() {
-
-    return <div>
-    <div>Welcome to your profile, {this.props.auth.user.name} </div>
-    <EventList userID={this.props.auth.user._id}/>
-    </div>
+    return <div>Welcome to your profile, {this.props.auth.user.name}</div>;
   }
 }
 
