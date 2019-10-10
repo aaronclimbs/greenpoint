5cdf0717c3ea26aa27db78f1d0eb0e17003d1812
Kevin Steele <kevdsteele@gmail.com>
2019-10-09 22:33:30 -0400

    Fixing epic fubar

--- routes/api/logs.js
+++ routes/api/logs.js
@@ -8,9 +8,23 @@ router.route("/")
 
 
 router
-  .route("/:id")
-  .get(logsController.findById)
+  .route("/:id/:date")
+  .get(logsController.findByIdDate)
   .put(logsController.update)
   .delete(logsController.remove);
+
+  router
+  .route("/:id")
+  .get(logsController.remove)
+  .put(logsController.update)
+  .delete(logsController.remove);
+
+  router
+  .route("/group/:id/:date")
+  .get(logsController.groupByEventDate)
+  .put(logsController.update)
+  .delete(logsController.remove);
+
+  
 
 module.exports = router;
