adbe1419906b5c549b98f88827a39de68eb6582b
Kevin Steele <kevdsteele@gmail.com>
2019-10-12 11:29:23 -0400

    Folder restructure

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import '../src/components/pages/FAQ/faq.css'
-import '../src/components/pages/404.css'
+
+
 
 import Root from "./Root";
 
