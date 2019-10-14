dd6cdac23c642c4af9dd3cd857cb08e5a48c551c
Kevin Steele <kevdsteele@gmail.com>
2019-10-14 14:53:59 -0400

    Adding tweak to Doughnut Chart

--- client/src/components/doughnut/index.js
+++ client/src/components/doughnut/index.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
-import {Doughnut} from 'react-chartjs-2';
+import {Doughnut, Chart} from 'react-chartjs-2';
 import "./style.css";
 
+
+
 class DoughnutChart extends Component {
+
+  
+
+  
  
   componentDidMount() {
+
+    var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
+Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
+  draw: function() {
+    originalDoughnutDraw.apply(this, arguments);
+    
+    var chart = this.chart.chart;
+    var ctx = chart.ctx;
+    var width = chart.width;
+    var height = chart.height;
+
+    var fontSize = (height / 250).toFixed(2);
+    ctx.font = fontSize + "em Verdana";
+    ctx.textBaseline = "middle";
+
+    var text = chart.config.data.points+ " pts",
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height / 1.73;
+
+    ctx.fillText(text, textX, textY);
+  }
+});
 
        
   }
   render() {
+
+
+
+    
 
     const noChartData = (
       <div>No results to display</div>
