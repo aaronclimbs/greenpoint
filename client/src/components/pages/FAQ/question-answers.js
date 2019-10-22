05f3e81a624c8fb60208c439b71d30482229a724
Kevin Steele <kevdsteele@gmail.com>
2019-10-22 12:58:23 -0400

    Adding FAQ text

--- client/src/components/pages/FAQ/question-answers.js
+++ client/src/components/pages/FAQ/question-answers.js
@@ -2,27 +2,27 @@ const qsAndAs = {
 
     question1: {
         question: "What is Greenpoint?",
-        answer: "Lorem ipsum dolor amet direct trade lo-fi sriracha street art farm-to-table mumblecore quinoa keytar typewriter adipisicing hammock venmo hella. Sartorial marfa vinyl salvia. Scenester truffaut pinterest, four loko ugh schlitz intelligentsia vinyl. Dolor typewriter paleo la croix chartreuse meditation before they sold out ethical unicorn mustache helvetica +1 next level pinterest. Next level reprehenderit af asymmetrical, sunt id sartorial woke."
+        answer: "Greenpoint is a site dedicated to increasing awareness of how our daily actions can have a positive impact on the environment. We take a gamification approach to encourage site users to compete with friends, family and colleagues."
     }, 
 
     question2: {
         question: "How do I get points?",
-        answer: "Pabst yr swag, woke blog dolore officia aesthetic. Brunch artisan dolore meggings selvage tbh selfies id. Ut cold-pressed pariatur art party fanny pack salvia. Proident tbh tousled fugiat sunt. Hell of waistcoat cloud bread poke taiyaki trust fund."
+        answer: "Points are earned by entering the green actions you complete each day. Simple right?! "
     }, 
 
     question3: {
         question: "What if my event isn't listed?",
-        answer: "Poke vegan mumblecore iceland, enamel pin vaporware williamsburg letterpress fugiat banjo. Seitan wolf succulents pop-up, raclette cliche VHS mumblecore. Raw denim thundercats pariatur irony aliqua ethical activated charcoal dreamcatcher health goth seitan umami shabby chic next level. Aute hashtag aliquip yr. Mustache typewriter laborum farm-to-table street art veniam man braid YOLO ethical lyft single-origin coffee. Consequat kale chips semiotics, et est quis elit."
+        answer: "We will be adding a Suggest Event option in a future release."
     }, 
 
     question4: {
-        question: "What badges can I get?",
-        answer: "Proident hashtag authentic jianbing, umami qui lomo literally dolore XOXO anim tbh chillwave ullamco franzen. Jianbing air plant selvage pop-up fanny pack magna microdosing meditation. Marfa semiotics microdosing fixie, excepteur veniam church-key ut. Chillwave hell of proident fam coloring book williamsburg selvage vexillologist hashtag etsy. Velit live-edge ullamco, godard man bun plaid tattooed. Live-edge skateboard reprehenderit kombucha adaptogen intelligentsia post-ironic."
+        question: "What awards can I get?",
+        answer: "Medals are earned monthly by reaching monthly point goals. Current Medals are Chocolate, Bronze, Silver and Gold. Additonal awards based on event catgeory points will be added in a future release. We also plan to engage with local retailers to provide discounts based on award levels."
     }, 
 
     question5: {
         question: "Why are you doing this?",
-        answer: "Plaid flexitarian shoreditch, flannel elit mixtape trust fund single-origin coffee reprehenderit meh tbh cronut magna disrupt. Hammock seitan hexagon magna irony dolore aesthetic. Glossier distillery af microdosing, hell of tempor in reprehenderit chambray tilde kale chips portland palo santo lorem. Godard swag vape tattooed fashion axe. Taiyaki tumblr est ennui, kitsch fugiat tote bag chicharrones iPhone polaroid meditation franzen quinoa in. Normcore lyft tumeric, 8-bit wayfarers echo park knausgaard typewriter."
+        answer: "Climate change is increasingly becoming an important issue that can not be ingored. We want to encourage people to engage in Green Actions by showing them how their daily action ADD up to a better world."
     }
 
 }
